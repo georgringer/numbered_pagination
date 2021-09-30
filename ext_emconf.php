738ad60fcb5b9401ca120bdb765790832d18b4ae
Stefan Froemken <froemken@gmail.com>
2021-09-30 16:42:26 +0200

    Update requirement of TYPO3 to version 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.12-11.0.99',
+            'typo3' => '10.4.12-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
