bf72d0bf6bb43464254a43584e73b9331b73ff16
Georg Ringer <georgringer@users.noreply.github.com>
2023-04-26 07:40:15 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.4',
+    'version' => '1.0.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '9.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
